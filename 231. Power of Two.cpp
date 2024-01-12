95639e92e7b25ef40287f29be036dcdacfe5887b
ADARSH SINGH <9792051262a@gmail.com>
2024-01-12 21:05:36 +0530

    Create 231. Power of Two.cpp

---
+++ 231. Power of Two.cpp
@@ -0,0 +1,19 @@
+// 231. Power of Two
+// Solution:- 
+
+// CODE:
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        if (n % 2 != 0) {
+            return false;
+        }
+        return isPowerOfTwo(n / 2);
+    }
+};
