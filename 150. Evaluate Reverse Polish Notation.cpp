be36910ba09cdc1cf2530294b6fb593fa08d9474
ADARSH SINGH <9792051262a@gmail.com>
2024-01-31 00:01:49 +0530

    Update 150. Evaluate Reverse Polish Notation.cpp

--- 150. Evaluate Reverse Polish Notation.cpp
+++ 150. Evaluate Reverse Polish Notation.cpp
@@ -1,1 +1,40 @@
 // 150. Evaluate Reverse Polish Notation:
+// Solution:-
+
+// CODE:
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> operands;
+
+        for (const string& token : tokens) {
+            if (isOperator(token)) {
+                int operand2 = operands.top();
+                operands.pop();
+                int operand1 = operands.top();
+                operands.pop();
+
+                if (token == "+")
+                    operands.push(operand1 + operand2);
+                else if (token == "-")
+                    operands.push(operand1 - operand2);
+                else if (token == "*")
+                    operands.push(operand1 * operand2);
+                else if (token == "/") {
+                    if (operand2 == 0)
+                        throw logic_error("Division by zero");
+                    operands.push(operand1 / operand2);
+                }
+            } else {
+                operands.push(stoi(token));
+            }
+        }
+
+        return operands.top();
+    }
+
+private:
+    bool isOperator(const string& token) {
+        return token == "+" || token == "-" || token == "*" || token == "/";
+    }
+};
