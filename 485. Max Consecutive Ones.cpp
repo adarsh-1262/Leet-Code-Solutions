bb6e0901152c025f3632d897a99b89f24254e615
ADARSH SINGH <9792051262a@gmail.com>
2024-03-05 23:37:26 +0530

    Array

---
+++ 485. Max Consecutive Ones.cpp
@@ -0,0 +1,21 @@
+// 485. Max Consecutive Ones
+// Solution:
+
+// CODE:
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt = 0;
+        int maximum = 0; 
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 1){
+                cnt++;
+            }else{
+                cnt = 0;
+            }
+            maximum = max(maximum, cnt);
+        }
+        return maximum;
+    }
+};
